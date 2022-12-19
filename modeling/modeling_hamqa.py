133c87ff185f83a3aa7790444593ee3a776929f1
Qing145 <zqg.zhang@hotmail.com>
2022-12-19 16:08:14 +0800

    upload

--- modeling/modeling_hamqa.py
+++ modeling/modeling_hamqa.py
@@ -33,22 +33,6 @@ def get_dim_act_curv(args):
     return dims, acts, curvatures
 
 
-class HyperGAT(nn.Module):
-    def __init__(self, in_features, out_features, c_in, c_out, dropout, act, use_bias, use_att, local_agg):
-        manifold = Hyperboloid
-        super(HyperGAT, self).__init__()
-        self.linear = HypLinear(manifold, in_features, out_features, c_in, dropout, use_bias)
-        self.agg = HypAgg(manifold, c_in, out_features, dropout, use_att, local_agg)
-        self.hyp_act = HypAct(manifold, c_in, c_out, act)
-
-    def forward(self, input):
-        x, adj = input
-        h = self.linear.forward(x)
-        h = self.agg.forward(h, adj)
-        h = self.hyp_act.forward(h)
-        output = h, adj
-        return output
-
 
 class HypLinear(nn.Module):
     def __init__(self, manifold, in_features, out_features, c, dropout, use_bias):
@@ -122,6 +106,22 @@ class HypAgg(Module):
     def extra_repr(self):
         return 'c={}'.format(self.c)
 
+
+class HyperGAT(nn.Module):
+    def __init__(self, in_features, out_features, c_in, c_out, dropout, act, use_bias, use_att, local_agg):
+        manifold = Hyperboloid
+        super(HyperGAT, self).__init__()
+        self.linear = HypLinear(manifold, in_features, out_features, c_in, dropout, use_bias)
+        self.agg = HypAgg(manifold, c_in, out_features, dropout, use_att, local_agg)
+        self.hyp_act = HypAct(manifold, c_in, c_out, act)
+
+    def forward(self, input):
+        x, adj = input
+        h = self.linear.forward(x)
+        h = self.agg.forward(h, adj)
+        h = self.hyp_act.forward(h)
+        output = h, adj
+        return output
 
 class HypAct(Module):
     def __init__(self, manifold, c_in, c_out, act):
